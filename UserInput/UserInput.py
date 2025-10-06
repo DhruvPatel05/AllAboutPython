413f3c11ea254d66b8c073247d50cc73e25a3871
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-06 14:38:58 -0700

    User input demo

---
+++ UserInput/UserInput.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+print("Hello, " + name)
+
+#exercise
+student_name = input("What is your name? ")
+favourite_color = input("What is your favourite color? ")
+print(student_name + " likes " + favourite_color)
