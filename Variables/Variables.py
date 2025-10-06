66b66850f1fe685e865e3eac096c7fcf5753d162
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-06 14:28:06 -0700

    Variables demo and definition

---
+++ Variables/Variables.py
@@ -0,0 +1,19 @@
+price = 10
+print(price)
+price = 20
+rating = 4.9
+name = 'Dhruv Patel'
+is_published = True
+print(price)
+print(rating)
+print(name)
+print(is_published)
+print('\n')
+
+#Exercise
+patient_name = 'John Smith'
+patient_age = 20
+is_newPatient = True
+print(patient_name)
+print(patient_age)
+print(is_newPatient)
