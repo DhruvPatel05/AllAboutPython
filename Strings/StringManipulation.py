8a795889d1ded89d9d3c74e094dfcf7e84070cd0
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-06 19:07:58 -0700

    Normal String Operations

---
+++ Strings/StringManipulation.py
@@ -0,0 +1,21 @@
+course = "Python's course for beginners"
+print(course)
+new_course = 'Python for "beginners"'
+print(new_course)
+emailString = '''Hi
+David
+Hope You are doing well
+'''
+print(emailString)
+print(course[0])
+print(course[-1])
+print(course[-2])
+print(course[0:2])
+print(course[0:-2])
+print(course[0:])
+print(course[1:])
+print(course[:5])
+print(course[:-1])
+
+name = 'Jennifer'
+print(name[1:-1])
