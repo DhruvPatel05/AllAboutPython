b8eb21970341b9e946fd7bb9cfb77f2d526f6c18
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-06 18:47:34 -0700

    Basic Mathematical Calculation

---
+++ Calculation/Mathematics.py
@@ -0,0 +1,11 @@
+birth_year = input("Enter birth year: ")
+print(birth_year)
+age = 2025 - int(birth_year)
+print(age)
+
+#Exercise
+
+weight = input("Enter weight in (lbs): ")
+print(weight)
+weight_kilograms = float(weight) * 0.45359237
+print(weight_kilograms)
