844d3ac8ceb5b9d15ba9da443f2a8146e4453a32
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-08 11:01:50 -0700

    Formatted String

---
+++ Formatted Strings/FormattedString.py
@@ -0,0 +1,8 @@
+from pyexpat.errors import messages
+
+first = 'David'
+last = 'Smith'
+message = first + ' [' + last + '] is a coder'
+print(message)
+msg = f'{first} [{last}] is a coder'
+print(msg)
