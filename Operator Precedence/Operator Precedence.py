9c32e90f5c71ed9e0751427d5471f709e6d94938
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-08 11:38:58 -0700

    Basic Operator Precedence

---
+++ Operator Precedence/Operator Precedence.py
@@ -0,0 +1,14 @@
+x = 10 + 3 * 2
+print(x)
+# parenthesis
+# exponential
+# Multiplication or Division
+# Subtraction or addition
+y = 10 + 3 * 2 ** 3
+print(y)
+
+z = (10 + 3) * 2 ** 3
+print(z)
+
+w = (10 + 3) * 2 - 3
+print(w)
