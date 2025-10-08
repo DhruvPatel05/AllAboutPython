ab01d1ac52b3b3f99b246e7528f5daaddabb53b7
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-08 12:22:31 -0700

    Basics of If Statement

---
+++ If Statement/If Statement.py
@@ -0,0 +1,21 @@
+is_hot = True
+is_cold = False
+if is_hot:
+    print("Is hot today?")
+    print("Drink Plenty of Water")
+elif is_cold:
+    print("It's a Cold day")
+    print("Wear warm clothes")
+else:
+    print("It's Lovely day")
+print("Enjoy your day")
+
+#Exercise
+
+price_House = 1000000
+is_GoodCredit = True
+if is_GoodCredit:
+    down_payment =  price_House * 0.10
+else:
+    down_payment = price_House * 0.20
+print(f"Down payment: ${down_payment}")
