6cd6c279592e88738eb3f620045dbce2a4ff9a27
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-08 11:18:02 -0700

    Different String Method Demo

---
+++ String Methods/StringMethods.py
@@ -0,0 +1,18 @@
+course = 'Python for Beginners'
+print(course)
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course)
+#Give index
+print(course.find('P'))
+print(course.find('Beginners'))
+
+# Replace character or word
+print(course.replace('Beginners','Absolute Beginners'))
+print(course.replace('beginners','Absolute Beginners'))
+print(course.replace('P','J'))
+
+# Boolean value
+print('Python' in course)
+print('python' in course)
