b0be7292da04cc79f0da8619d86c682ddf961ac2
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-20 10:42:32 -0700

    For Loop

---
+++ ForLoops/ForLoop.py
@@ -0,0 +1,17 @@
+# for item in 'Python':
+# print(item)
+for component in ['Dhruv','Kushal','Jeet']:
+    print(component)
+for digit in [1,2,3,4,5]:
+    print(digit)
+for i in range(10):
+    print(i)
+for num in range(5,10):
+    print(num)
+for number in range(10,20,3):
+    print(number)
+Prices = [10,20,30,40]
+total = 0
+for price in Prices:
+    total += price
+print(f"Total Prices: {total}")
