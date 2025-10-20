7145f342be7005f7aa0727189d826ce16c2c436e
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-20 11:31:32 -0700

    List Demo

---
+++ List/List.py
@@ -0,0 +1,10 @@
+names = ['John','Bob','Mosh','Sarah','Mary']
+print(names)
+print(len(names))
+print(names[0])
+print(names[-1])
+print(names[2:])
+print(names[2:4])
+print(names[:])
+names[0] = 'Jon'
+print(names)
