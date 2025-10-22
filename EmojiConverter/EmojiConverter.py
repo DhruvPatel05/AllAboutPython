660c43d23708815a41613084ac14300473d5b56b
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-22 12:40:38 -0700

    EmojiConverter updated with use of  Function

--- EmojiConverter/EmojiConverter.py
+++ EmojiConverter/EmojiConverter.py
@@ -1,11 +1,25 @@
 message = input(">")
-messages = message.split(' ')
-# print(messages)
-emojis = {
-    ":)" : "😊",
-    ":(" :"😞"
-}
-output = ""
-for word in messages:
-    output += emojis.get(word, word) + " "
-print(output)
+# messages = message.split(' ')
+# # print(messages)
+# emojis = {
+#     ":)" : "😊",
+#     ":(" :"😞"
+# }
+# output = ""
+# for word in messages:
+#     output += emojis.get(word, word) + " "
+# print(output)
+def emojiconverter(strmessage):
+    messages = strmessage.split(' ')
+    # print(messages)
+    emojis = {
+        ":)": "😊",
+        ":(": "😞"
+    }
+    output = ""
+    for word in messages:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+print(emojiconverter(message))
