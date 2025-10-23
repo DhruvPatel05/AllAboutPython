ab636f3afec5d97198f2191d4afc605ed78c74ee
Dhruv Patel <dhruvpatel913@gmail.com>
2025-10-22 17:50:24 -0700

    Packages

--- FindMaximum/App.py
+++ FindMaximum/App.py
@@ -1,4 +1,7 @@
 import FindMax
+import ecommerce.Shipping
+from ecommerce import Shipping
 numbers = [13,45,66,4,32,78,9,33]
 maxValue = FindMax.findMax(numbers)
 print(maxValue)
+ecommerce.Shipping.calc_ShippingCost()
---
+++ FindMaximum/ecommerce/Shipping.py
@@ -0,0 +1,4 @@
+def calc_ShippingCost():
+    print("Calculating Shipping Cost")
+
+
